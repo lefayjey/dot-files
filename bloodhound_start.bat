e7b81699ce200cec580c0f9eb64fb1e1c980d19b
lefayjey <lefayjey@lefayjey.fr>
2022-06-08 02:21:29 +0200

    Update 08062022

--- bloodhound_start.bat
+++ bloodhound_start.bat
@@ -1,3 +1,3 @@
-START /B C:\Cybersecurity\Tools\windows\neo4j\bin\neo4j.bat console
+START /MIN CMD.EXE /C C:\Cybersecurity\Tools\windows\neo4j\bin\neo4j.bat console
 timeout /t 10
 START /B C:\Cybersecurity\Tools\windows\BloodHound\Bloodhound.exe
